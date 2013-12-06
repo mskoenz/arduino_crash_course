61d6f5a5b6737221848d5286584c9dae2983cb31
Mario S. Könz <mskoenz@gmx.net>
2013-12-06 19:41:03 +0100

    35.Commit: added lots of comments to W8

--- program/acc_recap/W8_01_tictactoe_board_static/tictactoe.cpp
+++ program/acc_recap/W8_01_tictactoe_board_static/tictactoe.cpp
@@ -28,26 +28,30 @@ class program {
 public:
     program(): ioexp_(i2c_add)
             , led_(ioexp_)
-            , game(0.3)
+            , game(0.3) //chance of making a random move instead of the optimal
             , last_key_(0) {
         
-        randomSeed(analogRead(A3));
+        randomSeed(0); //doesn't matter since the device is always on and never resetted
         setup();
     }
     void setup() {
-        ustd::cout.init();
+        ustd::cout.init(); //only for debugging
         com::i2c_begin();
-        ioexp_.write(0xFF);
-    //------------------- define music for victory -------------------
-        pinMode(piezo, INPUT);
-        digitalWrite(piezo, piezo_down);
+        ioexp_.write(0xFF); //turn all lights off on the PCF
+    
+    //------------------- define music for victorys -------------------
+        pinMode(piezo, INPUT); //important. We dont want some currents to flow. tone ignores this anyway
+        digitalWrite(piezo, piezo_down); //the piezo uses up to 60 ma if the potential is not equal on both ends
+        
         music_[ttt::player].set_melody({note::c5, note::f5, note::a5, note::c6}, {1, 1, 1, 2});
         music_[ttt::ai].    set_melody({note::c5, note::c5, note::c5, note::c4}, {1, 1, 1, 2});
         music_[ttt::draw].  set_melody({note::f5, note::f5}, {2, 3});
+    
     //------------------- init game -------------------
         free_ = game.free();
         choice_ = 0;
-        led_.set(2*free_[choice_]+1, state::fast);
+        //0,2,4... are the red leds and 1,3,5... are the green leds -> 2*i+1
+        led_.set(2*free_[choice_]+1, state::fast); //switch on the selection led
     }
     void update() {
         tool::clock.update();
@@ -60,72 +64,69 @@ public:
     void loop() {
         update();
         
-        if(btn1_ == state::falling) {
-            last_key_ = tool::clock.millis();
+        if(btn1_ == state::falling) { //move selection
+            last_key_ = tool::clock.millis(); //reset sleep-timer
             noise(note::c4, 100);
             
-            led_.set(2*free_[choice_]+1, state::off);
+            led_.set(2*free_[choice_]+1, state::off); //turn off current selection
             
-            if(choice_ == 0)
+            if(choice_ == 0) //check bounds
                 choice_ = free_.size() - 1;
             else
                 --choice_;
             
-            led_.set(2*free_[choice_]+1, state::fast);
+            led_.set(2*free_[choice_]+1, state::fast); //turn on new selection
         }
-        if(btn3_ == state::falling) {
-            last_key_ = tool::clock.millis();
+        if(btn3_ == state::falling) { //move selection
+            last_key_ = tool::clock.millis(); //reset sleep-timer
             noise(note::d4, 100);
             
             
-            led_.set(2*free_[choice_]+1, state::off);
+            led_.set(2*free_[choice_]+1, state::off); //turn off current selection
             
-            if(choice_ == free_.size() - 1)
+            if(choice_ == free_.size() - 1) //check bounds
                 choice_ = 0;
             else
                 ++choice_;
                 
-            led_.set(2*free_[choice_]+1, state::fast);
+            led_.set(2*free_[choice_]+1, state::fast); //turn on new selection
         }
-        if(btn2_ == state::falling) {
-            last_key_ = tool::clock.millis();
+        if(btn2_ == state::falling) { //confirm selection
+            last_key_ = tool::clock.millis(); //reset sleep-timer
             noise(note::c5, 100);
             
-            led_.set(2*free_[choice_]+1, state::off);
+            led_.set(2*free_[choice_]+1, state::on); //turn on current selection
+            update(); //to see the lights, since we don't reach the update() fct until after the ai did the move
             
             game.player_move(free_[choice_]);
             
-            for(uint8_t i = 0; i < 9; ++i) {
-                if(game[i] == ttt::player)
-                    led_.set(2*i + 1, state::on);
-            }
-            update(); //to see the lights
             
-            if(game.win() == ttt::unknown) {
-                game.ai_move();
+            
+            if(game.win() == ttt::unknown) { //only let ai play if game is not over yet
                 noise(note::c6, 100);
-                for(uint8_t i = 0; i < 9; ++i) {
+                game.ai_move();
+                for(uint8_t i = 0; i < 9; ++i) { //go through all fileds and turn on ai-lights
                     if(game[i] == ttt::ai)
                         led_.set(2*i, state::on);
                 }
-                update(); //to see the lights
+                update(); //again, to see the lights
             }
             
             
-            if(game.win() == ttt::unknown) {
+            if(game.win() == ttt::unknown) { //only light up the selection if game is still on
                 free_ = game.free();
                 choice_ = 0;
                 led_.set(2*free_[choice_]+1, state::fast);
             }
         }
-        if(game.win() != ttt::unknown) {
-            music_[game.win()].play_blocking();
+        if(game.win() != ttt::unknown) { //game is over
+            music_[game.win()].play_blocking(); //victory theme
             game.reset();
             
-            for(uint8_t i = 0; i < 18; ++i) {
+            for(uint8_t i = 0; i < 18; ++i) { //turn off entire board
                 led_.set(i, state::off);
             }
-            
+            //create the selection again
             free_ = game.free();
             choice_ = 0;
             led_.set(2*free_[choice_]+1, state::fast);
@@ -134,37 +135,37 @@ public:
         if(tool::clock.millis() - last_key_ > 20000) {
             last_key_ = tool::clock.millis();
             
-            for(uint8_t i = 0; i < 18; ++i) {
+            for(uint8_t i = 0; i < 18; ++i) { //trun off all lights
                 led_.set(i, state::off);
             }
-            update();
+            update(); //actually does this
             
             noise(note::c3, 500);
             
             util::sleep(interrupt);
             
-            for(uint8_t i = 0; i < 9; ++i) {
+            noise(note::c6, 500);
+            for(uint8_t i = 0; i < 9; ++i) { //if back, light up the board according to the state
                 if(game[i] == ttt::player)
                     led_.set(2*i + 1, state::on);
                 if(game[i] == ttt::ai)
                     led_.set(2*i, state::on);
             }
             update();
-            led_.set(2*free_[choice_]+1, state::fast);
-            noise(note::c6, 500);
+            led_.set(2*free_[choice_]+1, state::fast); //and the selection
         }
     }
     void noise(int const & note, int const & dur) {
         tone(piezo, note, dur);
-        delay(dur-10);
+        delay(dur-10); //-10 for clearer cuts
         noTone(piezo);
-        digitalWrite(piezo, piezo_down);
+        digitalWrite(piezo, piezo_down); //saves tons of energy
     }
-    void receive(int n) {}
-    void request() {}
+    void receive(int n) {} //have to be here for the i2c module (I will change this soon)
+    void request() {} //have to be here for the i2c module (I will change this soon)
 private:
-    device::PCF8574_class ioexp_;
-    led_class led_;
+    device::PCF8574_class ioexp_; //the expander
+    led_class led_; //all 18 leds
     tool::button_class<tool::intern<btn1>, LOW> btn1_;
     tool::button_class<tool::intern<btn2>, LOW> btn2_;
     tool::button_class<tool::intern<btn3>, LOW> btn3_;
@@ -175,7 +176,7 @@ private:
     ttt::game_class game;
     tool::melody_class<tool::intern<piezo>> music_[3];
     
-    double last_key_;
+    double last_key_; //for the sleep-timeout
 };
 
 #include <main.hpp>
