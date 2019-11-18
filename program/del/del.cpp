cc7031e827dd13211cb9ce84f6d758f6e635e6a8
mskoenz <mskoenz@gmx.net>
2019-11-18 10:00:33 +0100

    del change

--- program/del/del.cpp
+++ program/del/del.cpp
@@ -11,36 +11,11 @@
 #include <diag.hpp>
 #include <tool.hpp>
 
-
-quint16_t test_0() {
-    uint16_t res = 0;
-    volatile uint16_t add = 1;
-    for(uint16_t i = 0; i < 1000; ++i) {
-        add = 1;
-        res += add;
+void digiwrite() {
+    for(uint32_t i = 0; i < 16; ++i) {
+        digitalWrite(13, HIGH);
     }
-    return res;
 }
-uint16_t test_1() {
-    uint16_t res = 0;
-    volatile uint16_t add = 1;
-    for(uint16_t i = 0; i < 1000; ++i) {
-        add = micros();
-        res += add;
-    }
-    return res;
-}
-uint16_t test_2() {
-    tool::clock.update();
-    volatile uint16_t add = 1;
-    uint16_t res = 0;
-    for(uint16_t i = 0; i < 1000; ++i) {
-        add = tool::clock.micros();
-        res += add;
-    }
-    return res;
-}
-
 
 class program {
 public:
@@ -49,23 +24,27 @@ public:
     }
     void setup() {
         ustd::cout.init(230400);
+        pinMode(6, OUTPUT);
+        analogWrite(6, 125);
         ustd::cout << F("start del.cpp") << ustd::endl;
-        led_ = state::blink;
+        led_ = state::on;
+        duty_ = 0;
     }
     void update() {
         tool::clock.update();
-        tool::clock.delay_to_hz(250);
-        led_.update();
+        // tool::clock.delay_to_hz(5000);
+        // led_.update();
+        // duty_ = (tool::clock.sec()*500)%10000;
+        // // std::cout << duty_ << std::endl;
+        // led_ = tool::clock.usec(10000) < duty_;
     }
     void loop() {
         update();
-        diag::benchmark_report(test_0, 10);
-        diag::benchmark_report(test_1, 10);
-        diag::benchmark_report(test_2, 10);
-        // diag::benchmark_report(millis, 100000);
+        // diag::speed_report();
     }
 private:
-    tool::light_class<D13, LOW> led_;
+    tool::light_class<D13, HIGH> led_;
+    int duty_;
 };
 
 #include <main.hpp>
